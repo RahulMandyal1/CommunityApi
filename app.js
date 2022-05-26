1862632d352a9bd5747b2c2e36560fe03d22910b
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-05-26 21:54:02 +0530

    hosted community api

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 // establishing the connection with the database
-require("../checkpointApi/config/database").connect();
+mongoose.connect(
+  "mongodb+srv://communityapi:communityapialtcampus@cluster0.v6x9g.mongodb.net/?retryWrites=true&w=majority",
+  (err) => {
+    console.log(err ? err : "Connection is made sucessfully");
+  }
+);
+
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -20,13 +27,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/v1/", indexRouter);
-app.use("/api/v1/users", usersRouter);
-app.use("/api/v1/profile", profileRouter);
-app.use("/api/v1/questions", questionRouter);
-app.use("/api/v1/answers", answersRouter);
+app.use("https://communityfourm.vercel.app/api/v1/", indexRouter);
+app.use("https://communityfourm.vercel.app/api/v1/users", usersRouter);
+app.use("https://communityfourm.vercel.app/api/v1/profile", profileRouter);
+app.use("https://communityfourm.vercel.app/api/v1/questions", questionRouter);
+app.use("https://communityfourm.vercel.app/api/v1/answers", answersRouter);
 
 //only admin can have access to these routes
 app.use(auth.isadmin);
-app.use("/api/v1/admindashboard", adminRouter);
+app.use("https://communityfourm.vercel.app/api/v1/admindashboard", adminRouter);
 module.exports = app;
--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //establish our application connection with the database
 module.exports.connect = function () {
-  mongoose.connect("mongodb+srv://rahulmandyal:<password>@cluster0.v6x9g.mongodb.net/?retryWrites=true&w=majority", (err) => {
+  mongoose.connect("mongodb+srv://communityapi:communityapialtcampus@cluster0.v6x9g.mongodb.net/?retryWrites=true&w=majority", (err) => {
     console.log(err ? err : "Connection is made sucessfully");
   });
 };
